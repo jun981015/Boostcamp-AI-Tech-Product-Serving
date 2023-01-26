6179916a16c1303cfa70d44574bcc7b92336aadc
홍준형 <89966626+jun981015@users.noreply.github.com>
2023-01-26 15:13:56 +0900

    Update app.py

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - github action Deploy Test2")
+    st.title("Mask Classification Model - github action Deploy Test3")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
